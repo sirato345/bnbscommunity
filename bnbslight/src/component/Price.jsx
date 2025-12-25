511deafc251b4ec21e8f8dbd082c40e81b70ceaa
shirato norimichi <sirato345@gmail.com>
2025-12-25 23:24:51 +0900

    CROS问题解决

--- bnbslight/src/component/Price.jsx
+++ bnbslight/src/component/Price.jsx
@@ -8,11 +8,13 @@ import "./Price.css";
 function Price() {
   const [bnbsPrice, setBNBsPrice] = React.useState(null);
   const [bnbPrice, setBNBPrice] = React.useState(null);
+  const [rate, setRate] = React.useState(null);
   const [marketCap, setMarketCap] = React.useState(null);
 
   const BNBs_PRICE_API =
     "https://www.mexc.com/api/dex/v1/data/get_market_info?chain_id=56&pair_ca=0x74716187C587866EC151990e2f22806a160493F4&token_ca=0xC07ef1C7af6112C34A110809C6c8Efb343e63A64";
-  const BNB_PRICE_API = 'https://api.binance.com/api/v3/ticker/price?symbol=BNBUSDT';
+  const BNB_PRICE_API =
+    "https://api.binance.com/api/v3/ticker/price?symbol=BNBUSDT";
   // 调整axios配置
   const instance = axios.create({
     timeout: 30000, // 增加超时时间
@@ -22,37 +24,50 @@ function Price() {
   });
 
   const getBNBsPrice = async () => {
-    instance.get(BNBs_PRICE_API).then((res) => {
-      setBNBsPrice((res.data.data.token_price).toFixed(6));
-      setMarketCap(Math.trunc(res.data.data.circulate_mkt_cap));
-    });
+
+    // 方法2：使用allOrigins（纯前端）
+    const fetchWithAllOrigins = async (url) => {
+      const allOriginsUrl = `https://api.allorigins.win/get?url=${encodeURIComponent(
+        url
+      )}`;
+
+      const response = await instance.get(allOriginsUrl);
+      const contents = JSON.parse(response.data.contents);
+      setBNBsPrice(contents.data.token_price.toFixed(6));
+      setRate(Math.trunc(bnbPrice / bnbsPrice));
+      setMarketCap(Math.trunc(contents.data.circulate_mkt_cap));
+      // allOrigins返回的数据结构是 { contents: "..." }
+      return contents;
+    };
+
+    // 示例：获取GitHub用户信息
+    fetchWithAllOrigins(BNBs_PRICE_API).then((data) => console.log(data));
   };
 
   const getBNBPrice = async () => {
     instance.get(BNB_PRICE_API).then((res) => {
-      console.log("Get BNB price:" + res.data['price'])
-      setBNBPrice((Number(res.data['price'])).toFixed(2));
+      console.log("Get BNB price:" + res.data["price"]);
+      setBNBPrice(Number(res.data["price"]).toFixed(2));
     });
   };
 
   const refresh = (e) => {
-      getBNBsPrice()
-      getBNBPrice()
+    getBNBsPrice();
+    getBNBPrice();
   };
 
   const divRef = useRef(null);
   useEffect(() => {
     if (divRef.current) {
-      getBNBsPrice()
-      getBNBPrice()
+      getBNBsPrice();
+      getBNBPrice();
     }
   });
 
   return (
     <div ref={divRef}>
       <table className="Price-table">
-        <tr className="Price-tr2">
-        </tr>
+        <tr className="Price-tr2"></tr>
         <tr className="Price-tr">
           <td className="Price-td">
             <img src={BNBLogo} alt="BNBLogo"></img>
@@ -76,7 +91,7 @@ function Price() {
             <span className="Price-span2">1 BNB = </span>
           </td>
           <td>
-            <span>{Math.trunc(bnbPrice / bnbsPrice)} BNBs</span>
+            <span>{rate} BNBs</span>
           </td>
         </tr>
         <tr className="Price-tr">
