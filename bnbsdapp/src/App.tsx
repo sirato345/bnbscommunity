d9a7b6a184b1d201367931e6add4f79ad8ae6da7
shirato345 <shirato345@gmail.com>
2026-04-17 12:15:10 +0900

    fee

--- bnbsdapp/src/App.tsx
+++ bnbsdapp/src/App.tsx
@@ -11,7 +11,7 @@ const BSC_CHAIN_ID = 56; // BSC
 const basicFeeConfig: WidgetFeeConfig = {
   name: "BNBs DApp fee",
   logoURI: "BNBs.svg",
-  fee: 0.0005,
+  fee: 0,
   showFeePercentage: true,
   showFeeTooltip: true,
   // 0.000  2.18	基础费用（一个BNB，870u的情况下，约千分之二点五）
