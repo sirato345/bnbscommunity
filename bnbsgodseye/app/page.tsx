918b024a564cf574d0cfe8970d7a861fe2a37419
shirato345 <shirato345@gmail.com>
2026-08-01 12:03:27 +0900

    no loading

--- bnbsgodseye/app/page.tsx
+++ bnbsgodseye/app/page.tsx
@@ -240,10 +240,10 @@ export default function CryptoScreenerPage() {
             zIndex: 999999,
           }}
         >
-          <div className="text-center">
+          {/* <div className="text-center">
             <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-600 mb-4 mx-auto"></div>
             <p className="text-gray-600">Loading...</p>
-          </div>
+          </div> */}
         </div>
       )}
 
